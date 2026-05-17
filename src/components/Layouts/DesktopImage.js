e2677df61680fff491afb5c557ffadca5748cfa9
Gabriel M <tochimike5@gmail.com>
2026-05-18 00:01:44 +0100

    increasing the text size

--- src/components/Layouts/DesktopImage.js
+++ src/components/Layouts/DesktopImage.js
@@ -84,7 +84,7 @@ export default function DesktopImage() {
               <button
                 key={tab}
                 onClick={() => setActiveTab(tab)}
-                className={`px-5 py-2 rounded-full text-sm uppercase tracking-[0.2em] transition-all duration-300
+                className={`px-5 py-2 rounded-full text-lg uppercase tracking-[0.2em] transition-all duration-300
                   
                   ${
                     activeTab === tab
